4f508ca10a132382bbbc204a38420cb963611fe2
Xiaohan Chen <chernxh@tamu.edu>
2019-11-11 13:52:10 +0100

    new postprocessing net with 1d conv

--- models/postprocessing.py
+++ models/postprocessing.py
@@ -45,3 +45,57 @@ class PostProcessingNet(nn.Module):
         out = self.layer2(out)
         return out
 
+
+class PostProcessingNetConv1d(nn.Module):
+    # def __init__(self, out_dim=None, hidden_dim=None, dataset='MiniImageNet', task_embedding='None'):
+    def __init__(self):
+        super(PostProcessingNetConv1d, self).__init__()
+        # self.out_dim = out_dim
+        # self.hidden_dim = hidden_dim
+        # self.dataset = dataset
+        # self.task_embedding = task_embedding
+
+        # if 'imagenet' in self.dataset.lower():
+        #     self.in_dim = 16000
+        #     self.dropout = 0.2
+        # elif 'cifar' in self.dataset.lower() or 'FS' in self.dataset:
+        #     self.in_dim = 2560
+        #     self.dropout = 0.2
+        # elif 'omniglot' in self.dataset.lower():
+        #     raise NotImplementedError('Post processing module is not implemented for Omniglot dataset yet')
+        # else:
+        #     raise ValueError('Cannot recognize dataset {}'.format(self.dataset))
+
+        # if self.task_embedding != 'None':
+        #     self.in_dim *= 2
+
+        # if self.out_dim is None:
+        #     self.out_dim = self.in_dim
+        # if self.hidden_dim is None:
+        #     self.hidden_dim = self.out_dim
+
+        self.layer1 = nn.Sequential(
+            nn.Conv1d(in_channels=2, out_channels=4, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(4),
+            nn.ReLU(),
+            nn.MaxPool1d(2)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv1d(in_channels=4, out_channels=8, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm1d(8),
+            nn.ReLU(),
+            nn.MaxPool1d(2)
+        )
+        self.layer3 = nn.Sequential(
+            nn.Conv1d(in_channels=8, out_channels=16, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm1d(16)
+        )
+
+    def forward(self, x):
+        emb_sample, emb_task = x.split(x.size(1) // 2, dim=1) # (bs, d/2), (bs, d/2)
+        emb_fusion = torch.concat((emb_sample.unsqueeze(1), emb_task.unsqueeze(1)), dim=1) # (bs, 2, d/2)
+        out = self.layer1(emb_fusion)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        return out
+
--- train.py
+++ train.py
@@ -15,7 +15,7 @@ from models.R2D2_embedding import R2D2Embedding
 from models.protonet_embedding import ProtoNetEmbedding
 from models.ResNet12_embedding import resnet12
 from models.task_embedding import TaskEmbedding
-from models.postprocessing import PostProcessingNet
+from models.postprocessing import PostProcessingNet, PostProcessingNetConv1d
 
 from utils import set_gpu, Timer, count_accuracy, check_dir, log
 
@@ -120,9 +120,15 @@ def get_task_embedding_func(options):
 
 def get_postprocessing_model(options):
     # Choose the post processing network for embeddings
-    if options.post_processing:
+    if options.post_processing == 'FC':
         return PostProcessingNet(dataset=options.dataset, task_embedding=options.task_embedding).cuda()
-    else:
+    if options.post_processing == 'Conv1d':
+        postprocessing_net = PostProcessingNetConv1d().cuda()
+        if options.dataset == 'miniImageNet' or options.dataset == 'tieredImageNet':
+            device_ids = list(range(len(options.gpu.split(','))))
+            postprocessing_net = torch.nn.DataParallel(postprocessing_net, device_ids=device_ids)
+        return postprocessing_net
+    elif options.post_processing == 'None':
         return nn.Identity().cuda()
 
 if __name__ == '__main__':

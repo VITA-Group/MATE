c55f48c756f003794bb087715b3c1cb2fe199472
Xiaohan Chen <chernxh@tamu.edu>
2020-03-25 17:52:46 -0500

    Change the implementation of FiLM regularization

--- models/loss.py
+++ models/loss.py
@@ -5,20 +5,20 @@ import numpy as np
 from .FiLM import FiLM_Layer
 
 
-def get_film_loss(model, _task_emb, loss_type):
+def get_film_loss(model, task_emb, loss_type):
     if 'none' in loss_type.lower():
         return 0.0
     elif 'ortho' in loss_type.lower():
-        return get_film_srip_loss(model, _task_emb)
+        return get_film_srip_loss(model, task_emb)
     elif 'msgan' in loss_type.lower():
-        return get_film_msgan_loss(model, _task_emb)
+        return get_film_msgan_loss(model, task_emb)
     elif 'dsgan' in loss_type.lower():
-        return get_film_dsgan_loss(model, _task_emb)
+        return get_film_dsgan_loss(model, task_emb)
     else:
         raise ValueError('Invalid `loss_type` for FiLM regularization')
 
 
-def get_film_msgan_loss(model, _task_emb):
+def get_film_msgan_loss(model, task_emb):
     """Short summary.
 
     Parameters
@@ -35,18 +35,21 @@ def get_film_msgan_loss(model, _task_emb):
 
     """
 
-    assert _task_emb is not None
+    assert task_emb is not None
+    tasks_per_batch = task_emb.size(0) # (tasks_per_batch, 1, d)
+    task_emb_expanded = task_emb.expand(-1, tasks_per_batch, -1)
     film_loss = 0.0
     for m in model.modules():
         if isinstance(m, FiLM_Layer):
-            tasks_per_batch = _task_emb.size(0) # (tasks_per_batch, 1, d)
-            _film_out = m.get_mlp_output(_task_emb)
+            film_out = m.get_mlp_output(task_emb)
+            film_out_expanded = film_out.expand(-1, tasks_per_batch, -1)
 
-            _task_emb = _task_emb.expand(-1, tasks_per_batch, -1)
-            _film_out = _film_out.expand(-1, tasks_per_batch, -1)
-
-            d_input  = torch.dist(_task_emb, _task_emb.transpose(0,1), p = 1)
-            d_output = torch.dist(_film_out, _film_out.transpose(0,1), p = 1)
+            d_input  = torch.dist(task_emb_expanded,
+                                  task_emb_expanded.transpose(0,1),
+                                  p = 1)
+            d_output = torch.dist(film_out_expanded,
+                                  film_out_expanded.transpose(0,1),
+                                  p = 1)
             lz = d_output / d_input
             eps = 1 * 1e-5
             film_loss += 1 / (lz + eps)

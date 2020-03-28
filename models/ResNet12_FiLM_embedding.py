e954bd60b6be51cb4c85dd67369640547a88c7f3
Xiaohan Chen <chernxh@tamu.edu>
2020-03-28 09:34:12 -0500

    Remove Kaiming's initialization for FiLM layers

--- models/ResNet12_FiLM_embedding.py
+++ models/ResNet12_FiLM_embedding.py
@@ -130,8 +130,6 @@ class ResNet_FiLM(nn.Module):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='leaky_relu')
-            # elif isinstance(m, nn.Linear):
-            #     nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='leaky_relu')
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)

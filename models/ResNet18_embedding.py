2db88dd14e5b11fc8fbc72084c7dfac0c8ef4582
Xiaohan Chen <chernxh@tamu.edu>
2020-04-30 12:57:20 -0500

    Remove inplace keyword for ReLU

--- models/ResNet18_embedding.py
+++ models/ResNet18_embedding.py
@@ -32,7 +32,8 @@ class Bottleneck(nn.Module):
         self.bn2 = nn.BatchNorm2d(planes)
         self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(planes * 4)
-        self.relu = nn.ReLU(inplace=True)
+        # self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU()
         self.downsample = downsample
         self.stride = stride
 
